081c8d603e37af8af06432d8a9ff77bbb1a2daa1
heinstein <3276604+heinsteinh@users.noreply.github.com>
2024-11-16 22:58:22 +0100

    Fail CI build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ echo "Hello $@"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-echo "answer=42" >> $GITHUB_OUTPUT
+echo "answer=4" >> $GITHUB_OUTPUT
 
 echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
 echo "<h3> The time was :hearts: :hearts: :hearts: $time</h3>" >> $GITHUB_STEP_SUMMARY
