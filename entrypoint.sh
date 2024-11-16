d228ad5d43b2bd457661d779ec934ef720349379
heinstein <3276604+heinsteinh@users.noreply.github.com>
2024-11-16 23:11:15 +0100

    Working on env. variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ echo "Hello $@"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-echo "answer=4" >> $GITHUB_OUTPUT
+echo "answer=42" >> $GITHUB_OUTPUT
 
 echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
-echo "<h3> The time was :hearts: :hearts: :hearts: $time</h3>" >> $GITHUB_STEP_SUMMARY
+echo "<h3> The time was :email: $time</h3>" >> $GITHUB_STEP_SUMMARY
 echo "<h3> The answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
