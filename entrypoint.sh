a6b7cfc11cb9d675fe6b1b1bfaa7a5075f7107cf
heinstein <3276604+heinsteinh@users.noreply.github.com>
2024-11-16 22:55:43 +0100

    Update icon

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ echo "time=$time" >> $GITHUB_OUTPUT
 echo "answer=42" >> $GITHUB_OUTPUT
 
 echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
-echo "<h3> The time was :clock: $time</h3>" >> $GITHUB_STEP_SUMMARY
+echo "<h3> The time was :hearts: :hearts: :hearts: $time</h3>" >> $GITHUB_STEP_SUMMARY
 echo "<h3> The answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
